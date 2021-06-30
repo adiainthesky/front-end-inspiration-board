8f3501362d553ff5eec0925debff4ead3a2f1f1d
Nataliia Getlin <natali.getlin@gmail.com>
2021-06-30 15:56:36 -0700

    - CreateBoard: functional form submit;

--- src/App.js
+++ src/App.js
@@ -2,36 +2,56 @@ import React from 'react';
 import './App.css';
 import BoardList from './components/BoardList';
 import CreateBoard from './components/CreateBoard';
-import {useState} from 'react';
-// import axios from 'axios';
+import { useState } from 'react';
+import axios from 'axios';
 
 
 function App() {
-  const [boards, setBoards] = useState([{title:'Delia\'s', owner: "delia", cards: []}, {title:'Adelaide\'s'}, {title:'Natasha\'s'}, {title:'Sara\'s'}])
+  const [boards, setBoards] = useState(
+    [
+      {
+        title: 'Delia\'s',
+        owner: "delia",
+        board_id: null
+      },
+
+      {
+        title: 'Adelaide\'s',
+        owner: "adelaide",
+        board_id: null
+      },
+    ]
+  )
+
   const [selectedBoard, setSelectedBoard] = useState(null)
+  
 
   //function to get all boards at start of app
   //useEffect to get them all loaded the first time (with extra [])
   const getAllOptions = () => {
-    const options = boards.map(board => 
+    const options = boards.map(board =>
       // <option value={board.title} selected={isSelected(board.title)}>{board.title}</option>
       //not needed  if boards === selectedBoards then selected =true
       <option value={board.title}>{board.title}</option>
     );
-    return options;    
+    return options;
+  }
+
+  const updateBoardList = boards => {
+    setBoards(boards)
   }
 
   return (
     <div className="App">
       <header>
         {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpTs4k73tsmQGuUInH-lVvYy0S7yOhO4z3hQ&usqp=CAU" className="App-logo" alt="sandy beach" /> */}
-        <h1>{selectedBoard} Inspiration Board</h1> 
+        <h1>{selectedBoard} Inspiration Board</h1>
       </header>
 
       <main>
-        <BoardList getAllOptions={getAllOptions} setSelectedBoard={setSelectedBoard}/>
-        <CreateBoard setBoards={setBoards}/>
-      </main> 
+        <BoardList getAllOptions={getAllOptions} setSelectedBoard={setSelectedBoard} />
+        <CreateBoard onUpdateBoardList={updateBoardList} />
+      </main>
 
     </div>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,20 @@
 import PropTypes from 'prop-types';
 import './Board.css';
+import { useState } from 'react';
 
 const Board = () => {
+  // const [cards, setCards] = useState([])
+  const [cards, setCards] = useState(
+    [
+      {
+        board_id: null,
+        card_id: null,
+        likes_count: 0,
+        message: ""
+      },
+    ]
+  )
+
   return <div></div>
 };
 
